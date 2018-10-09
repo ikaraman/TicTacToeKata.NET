d5ca5a51013eb24a9ebce1dca53e0fbc612b600d
Ivan Karaman <ikaraman@ya.ru>
2018-10-09 16:52:04 +1300

    Created a simple "hello world" console app project

---
+++ TicTakToe/TheGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicTakToe
+{
+    internal static class TheGame
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Welcome to the TikTakToe game!");
+        }
+    }
+}
