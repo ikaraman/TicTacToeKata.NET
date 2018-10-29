ca6e1af4bc5a329c173f0b6703fe4dcf4ce44116
Ivan Karaman <ikaraman@ya.ru>
2018-10-29 20:55:39 +1300

    Added 1.0 version of the player class

---
+++ TicTakToe/src/Player.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace TicTakToe
+{
+    public static class Player
+    {
+        public static int GetCurrentPlayerNumber()
+        {
+            return 1;
+        }
+        
+        
+        public static char GetCurrentPlayerSymbol()
+        {
+            return 'X';
+        }
+    }
+}
