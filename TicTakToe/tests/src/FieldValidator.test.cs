c7077a45befe2aa8d5b5388b495453b2ce391ad0
Ivan Karaman <ikaraman@ya.ru>
2018-10-30 20:19:02 +1300

    Tests for the IsThereWinningColumn() method

--- TicTakToe/tests/src/FieldValidator.test.cs
+++ TicTakToe/tests/src/FieldValidator.test.cs
@@ -10,5 +10,39 @@ namespace TicTakToe
         {
             Assert.AreEqual(true, FieldValidator.IsFieldSizeValid("3"));
         }
+        
+        
+        [Test]
+        public void IsThereWinningColumnShouldBeTrueForFirstColumn()
+        {
+            Field.CreateField(3);
+            Field.UpdateCellState("1,1");
+            Field.UpdateCellState("2,1");
+            Field.UpdateCellState("3,1");
+            
+            Assert.AreEqual(true, FieldValidator.IsThereWinningColumn('X'));
+        }
+        
+        [Test]
+        public void IsThereWinningColumnShouldBeTrueForSecondColumn()
+        {
+            Field.CreateField(3);
+            Field.UpdateCellState("1,2");
+            Field.UpdateCellState("2,2");
+            Field.UpdateCellState("3,2");
+            
+            Assert.AreEqual(true, FieldValidator.IsThereWinningColumn('X'));
+        }
+        
+        [Test]
+        public void IsThereWinningColumnShouldBeTrueForThirdColumn()
+        {
+            Field.CreateField(3);
+            Field.UpdateCellState("1,3");
+            Field.UpdateCellState("2,3");
+            Field.UpdateCellState("3,3");
+            
+            Assert.AreEqual(true, FieldValidator.IsThereWinningColumn('X'));
+        }
     }
 }
